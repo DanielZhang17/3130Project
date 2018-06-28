a32aa8510115971644849f57dceb4e0f915b879b
Anran Zhang <zhanganran2011@Live.com>
2018-06-28 16:24:45 -0300

    updated contact us
    added actions to the the cardviews so that phone, email, browser, google maps can be pulled up from the contact us page.

--- app/src/main/java/com/project/csci3130/dalrs/ThirdFragment.java
+++ app/src/main/java/com/project/csci3130/dalrs/ThirdFragment.java
@@ -1,8 +1,11 @@
 package com.project.csci3130.dalrs;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +13,48 @@ import android.view.ViewGroup;
 public class ThirdFragment extends Fragment{
 
     View MyView;
+    CardView phone,email,web,address;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         MyView = inflater.inflate(R.layout.third_layout,container,false);
+        phone = MyView.findViewById(R.id.phone);
+        email = MyView.findViewById(R.id.email);
+        web = MyView.findViewById(R.id.web);
+        address = MyView.findViewById(R.id.address);
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9024942211"));
+                startActivity(intent);
+            }
+        });
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("mailto:support@dal.ca")
+                        .buildUpon()
+                        .build();
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                startActivity(Intent.createChooser(emailIntent, "Choose client"));
+            }
+        });
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dal.ca"));
+                startActivity(intent);
+            }
+        });
+        address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/search/?api=1&query=Dalhousie+University"));
+                startActivity(intent);
+            }
+        });
         return MyView;
     }
 }
