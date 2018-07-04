aa11ebdc29777320d61049b4cdc847224a1e7f24
jiaqiguo <422532105@qq.com>
2018-07-04 01:58:51 -0300

    Find bug in CourseViewList and fixed

--- app/src/main/java/com/project/csci3130/dalrs/CourseViewList.java
+++ app/src/main/java/com/project/csci3130/dalrs/CourseViewList.java
@@ -2,9 +2,11 @@ package com.project.csci3130.dalrs;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.ArrayMap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,15 +32,16 @@ public class CourseViewList extends AppCompatActivity {
     private TextView test2;
     private DatabaseReference mReference;
     private DatabaseReference wReference;
-//
     private ExpandableListView expandableListView;
     private ExpandableListViewAdapter adapter;
     private List<String> courseCS = new ArrayList<>();
     private List<String> courseSTAT = new ArrayList<>();
     private String[] groupList = new String[]{"Computer Science","Statistic"};
-    //private ArrayList<String> groupListView = new ArrayList<>();
-    //private String[] groupList;
     private Map<String, List<String>> courseList = new HashMap<>();
+    DatabaseReference ref;
+    Course course;
+    ArrayList<Course> courses1 = new ArrayList<Course>();
+    ArrayList<Course> coursesLec = new ArrayList<>();
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,20 @@ public class CourseViewList extends AppCompatActivity {
         setContentView(R.layout.activity_course_list_view_main);
 
         expandableListView = (ExpandableListView) findViewById(R.id.expandableList);
+        ref = FirebaseDatabase.getInstance().getReference("Courses");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    course = ds.getValue(Course.class);
+                    courses1.add(course);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         //iniUI();
         initData();
         listView();
@@ -60,120 +77,48 @@ public class CourseViewList extends AppCompatActivity {
     private void initData() {
         mReference = FirebaseDatabase.getInstance().getReference();
         DatabaseReference haha = mReference.child("Courses");
-        //DatabaseReference haha = mReference.child("Courses").child("10001").child("CourseDep");
 
         haha.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 collectCourseTitles((Map<String,Object>) dataSnapshot.getValue());
-                // collectCourseDep((Map<String,Object>) dataSnapshot.getValue());
-                //courseList = listView();
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
     }
-    /*private void initData1() {
-        wReference = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference lala = wReference.child("Courses");
-        //DatabaseReference haha = mReference.child("Courses").child("10001").child("CourseDep");
-
-        lala.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                //collectCourseTitles((Map<String,Object>) dataSnapshot.getValue());
-                collectCourseDep((Map<String,Object>) dataSnapshot.getValue());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }*/
-
-    /*private void iniUI() {
-        test1 = (TextView) findViewById(R.id.test1);
-        test2 = (TextView) findViewById(R.id.test2);
-    }*/
-    /*private void collectCourseDep(Map<String,Object> department){
-        for (Map.Entry<String, Object> entry : department.entrySet()) {
-
-            Map depart = (Map) entry.getValue();
-
-            String dep = (String) depart.get("CourseDep");
-
-            if(dep!=null && !groupListView.contains(dep)) {
-                groupListView.add(dep);
-            }
-        }
-        groupList = (String[]) groupListView.toArray(new String[groupListView.size()]);
-       // courseList.put(groupList[0],courseCS);
-       // courseList.put(groupList[1],courseSTAT);
-
-    }
-    */
-
     private void collectCourseTitles(Map<String,Object> courses){
-        int count = 0;
         String[] list =null;
         for(Map.Entry<String,Object> entry: courses.entrySet()){
 
             Map course = (Map) entry.getValue();
             boolean flag = false;
-
+/*
             String title = (String) course.get("CourseTitle");
             String dep = (String) course.get("CourseDep");
+            String type = (String) course.get("CourseType");
 
-            /*if(title != null && dep.contains("Computer")){
+            if(title != null && dep.equals("Computer Science") && !courseCS.contains(title) && type.equals("Lec")){
                 courseCS.add(title);
             }
-            if(title !=null && dep.contains("Stat")){
+            if(title !=null && dep.contains("Statistics") && !courseSTAT.contains(title) && type.equals("Lec")){
                 courseSTAT.add(title);
             }*/
-
-            if(title != null && dep.contains("Computer") && !courseCS.contains(title)){
-                courseCS.add(title);
-            }
-            if(title !=null && dep.contains("Stat") && !courseSTAT.contains(title)){
-                courseSTAT.add(title);
-            }
-
-            /*if(groupList==null){
-                groupList[0] = dep;
-                count++;
-            }
-            else {
-                for (int i = 0; i < count + 1; i++) {
-                    if (groupList == null) {
-                        break;
-                    }
-                    if (!groupList.equals(dep)) {
-                        flag = true;
-                    }
+            for(int i=0; i< courses.size(); i++){
+                if(courses1.get(i).getCourseType().contains("ec")){
+                    coursesLec.add(courses1.get(i));
                 }
-                if (flag == true) {
-                    groupList[count] = dep;
-                    count++;
-
+            }
+            for(int j=0; j< coursesLec.size(); j++){
+                if(coursesLec.get(j).getCourseDep().contains("Computer") && !courseCS.contains(coursesLec.get(j).getCourseTitle())){
+                    courseCS.add(coursesLec.get(j).getCourseTitle());
                 }
-            }*/
-            /*for(int i = 0; i<groupList.length; i++) {
-                if (title != null) {
-                    if(dep.equals(groupList[i])){
-                        courseView.get(i).add(title);
-                    }
+                else if(coursesLec.get(j).getCourseDep().contains("Stat") && !courseSTAT.contains(coursesLec.get(j).getCourseTitle())){
+                    courseSTAT.add(coursesLec.get(j).getCourseTitle());
                 }
-            }*/
-
-
+            }
         }
-
-        //test1.setText(groupList.length);
-        //test2.setText(groupList.toString());
     }
     private void listView(){
 
@@ -181,93 +126,6 @@ public class CourseViewList extends AppCompatActivity {
         courseList.put(groupList[1],courseSTAT);
 
     }
-    /*public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
-
-        @Override
-        //get one item from one groupList.
-        public Object getChild(int parentPosition, int childPosition){
-            return courseList.get(parentPosition).get(childPosition);
-        }
-
-        @Override
-        //Get the number of groups
-        public int getGroupCount(){
-            return courseList.size();
-        }
-
-        @Override
-        //Get the number of child elements in the specified group
-        public int getChildrenCount(int parentPosition){
-            return courseList.get(parentPosition).size();
-        }
-
-        @Override
-        //Get the data in the specified group
-        public Object getGroup(int parentPosition){
-            return courseList.get(parentPosition);
-        }
-
-        @Override
-        //Get the id of a child of a group
-        public long getChildId(int parentPosition, int childPosition){
-            return childPosition;
-        }
-
-        @Override
-        //Get the id of the specified group
-        public long getGroupId(int parentPosition){
-            return parentPosition;
-        }
-
-        @Override
-        public boolean hasStableIds(){
-            return false;
-        }
-
-        @Override
-        public View getGroupView(int parentPosition, boolean isExpand,
-                                 View convertView, ViewGroup viewGroup){
-            if(convertView == null){
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.group_item,null);
-            }
-            convertView.setTag(R.layout.group_item, parentPosition);
-
-            convertView.setTag(R.layout.child_item, -1);
-
-            TextView textView = (TextView) convertView.findViewById(R.id.groupView);
-
-            textView.setText(groupListView.get(parentPosition));
-
-            return convertView;
-        }
-
-        @Override
-        public View getChildView(int parentPosition, int childPosition, boolean isLastChild,
-                                 View convertView, ViewGroup viewGroup){
-            if(convertView == null){
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.child_item,null);
-            }
-            convertView.setTag(R.layout.group_item, parentPosition);
-
-            convertView.setTag(R.layout.child_item, childPosition);
-
-            TextView textView = (TextView) convertView.findViewById(R.id.childView);
-
-            textView.setText(courseList.get(parentPosition).get(childPosition));
-
-            return convertView;
-        }
-
-        @Override
-        public boolean isChildSelectable(int parentPosition, int childPosition){
-            return false;  //not in interaction 1
-        }
-    }*/
-
     public class ExpandableListViewAdapter extends BaseExpandableListAdapter{
 
         @Override
