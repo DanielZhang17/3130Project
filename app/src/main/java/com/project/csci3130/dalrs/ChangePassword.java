6a128047e0a64f6ad52eb27652879835d7685d6d
YahooRoarz <39277274+YahooRoarz@users.noreply.github.com>
2018-07-23 15:53:58 -0500

    Refactor the structure of ChangePassword.java(Yahu)

--- app/src/main/java/com/project/csci3130/dalrs/ChangePassword.java
+++ app/src/main/java/com/project/csci3130/dalrs/ChangePassword.java
@@ -35,7 +35,7 @@ private ActionBar Bar;
     private static FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
@@ -70,57 +70,65 @@ private ActionBar Bar;
                     Toast.makeText(getApplicationContext(),"Error!",Toast.LENGTH_LONG).show();
                     return;
                 }
-                final String newpassword1 = NewPassword1.getText().toString();
-                final String newpassword2 = NewPassword2.getText().toString();
-                String oldpassword = CurrentPassword.getText().toString();
-                user = FirebaseAuth.getInstance().getCurrentUser();
-                final String email = user.getEmail();
-                if(oldpassword.isEmpty()){
-                    CurrentPassword.setError("");
-                    Toast.makeText(getApplicationContext(),"Please enter your old password!",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(!newpassword1.equals(newpassword2)){
-                    NewPassword1.setError("");
-                    NewPassword2.setError("");
-                    Toast.makeText(getApplicationContext(),"Thw passwords does not match!",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(newpassword1.length()<8){
-                    NewPassword1.setError("");
-                    NewPassword2.setError("");
-                    Toast.makeText(getApplicationContext(),"Thw password need to be at least 8 characters long",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                final AuthCredential credential = EmailAuthProvider.getCredential(email,oldpassword);
-                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                ChangePassword();
+            }
+        });
+    }
+
+
+
+    public void ChangePassword(){
+
+        final String newpassword1 = NewPassword1.getText().toString();
+        final String newpassword2 = NewPassword2.getText().toString();
+        String oldpassword = CurrentPassword.getText().toString();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        final String email = user.getEmail();
+        if(oldpassword.isEmpty()){
+            CurrentPassword.setError("");
+            Toast.makeText(getApplicationContext(),"Please enter your old password!",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(!newpassword1.equals(newpassword2)){
+            NewPassword1.setError("");
+            NewPassword2.setError("");
+            Toast.makeText(getApplicationContext(),"The passwords does not match!",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(newpassword1.length()<8){
+            NewPassword1.setError("");
+            NewPassword2.setError("");
+            Toast.makeText(getApplicationContext(),"Thw password need to be at least 8 characters long",Toast.LENGTH_LONG).show();
+            return;
+        }
+        final AuthCredential credential = EmailAuthProvider.getCredential(email,oldpassword);
+        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    user.updatePassword(newpassword1).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                user.updatePassword(newpassword1).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (!task.isSuccessful()) {
-                                            Snackbar snackbar_fail = Snackbar
-                                                    .make(findViewById(R.id.result), "Something went wrong. Please try again later", Snackbar.LENGTH_LONG);
-                                            snackbar_fail.show();
-                                        } else {
-                                            Snackbar snackbar_su = Snackbar
-                                                    .make(findViewById(R.id.result), "Password Successfully Modified", Snackbar.LENGTH_LONG);
-                                            snackbar_su.show();
-                                        }
-                                    }
-                                });
+                            if (!task.isSuccessful()) {
+                                Snackbar snackbar_fail = Snackbar
+                                        .make(findViewById(R.id.result), "Something went wrong. Please try again later", Snackbar.LENGTH_LONG);
+                                snackbar_fail.show();
                             } else {
                                 Snackbar snackbar_su = Snackbar
-                                        .make(findViewById(R.id.result), "Authentication Failed", Snackbar.LENGTH_LONG);
+                                        .make(findViewById(R.id.result), "Password Successfully Modified", Snackbar.LENGTH_LONG);
                                 snackbar_su.show();
+                                startActivity(new Intent(ChangePassword.this,LoginInterfaceActivity.class));
+                                Toast.makeText(getApplicationContext(),"Password Successfully Modified",Toast.LENGTH_LONG).show();
                             }
                         }
                     });
-                    auth.signOut();
-                    startActivity(new Intent(ChangePassword.this,LoginInterfaceActivity.class));
-                 }
+                } else {
+                    Snackbar snackbar_su = Snackbar
+                            .make(findViewById(R.id.result), "Authentication Failed", Snackbar.LENGTH_LONG);
+                    snackbar_su.show();
+                }
+            }
         });
     }
 }
